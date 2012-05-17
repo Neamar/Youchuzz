ca95719ac3956309516cd892e62055abcb64a020
Neamar <neamar@neamar.fr>
2012-05-17 22:10:58 +0200

    Nouveau wireframing.
    Ajout du mockup API

---
+++ src/fr/youchuzz/API.java
@@ -0,0 +1,28 @@
+package fr.youchuzz;
+
+/**
+ * Communication avec l'API Youchuzz
+ * @author neamar
+ *
+ */
+public class API {
+	public static API getInstance()
+	{
+		
+	}
+	
+	private API()
+	{
+		
+	}
+	
+	/**
+	 * Récupère un token d'utilisation Youchuzz
+	 * @param facebookToken
+	 * @return youchuzzToken
+	 */
+	public String login(String facebookToken)
+	{
+		
+	}
+}
