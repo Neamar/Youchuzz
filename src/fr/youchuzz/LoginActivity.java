33582ef73f44e45d2ebf75e5584e1b474c39765a
Neamar <neamar@neamar.fr>
2012-05-17 14:34:29 +0200

    First wireframing

--- src/fr/youchuzz/LoginActivity.java
+++ src/fr/youchuzz/LoginActivity.java
@@ -8,6 +8,6 @@ public class LoginActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_create);
     }
 }
