c4b1f575b4426b1685dd339999b7be13d85a8edc
Neamar <neamar@neamar.fr>
2012-05-17 23:20:36 +0200

    Login stub

--- src/fr/youchuzz/LoginActivity.java
+++ src/fr/youchuzz/LoginActivity.java
@@ -4,6 +4,8 @@ import com.androidquery.AQuery;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class LoginActivity extends Activity {
 	/** Called when the activity is first created. */
@@ -16,6 +18,13 @@ public class LoginActivity extends Activity {
 		
 		setContentView(R.layout.activity_login);
 		
-
+		AQuery aq = new AQuery(this);
+		aq.id(R.id.login_facebook).clicked(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Log.w("youchuzz", "Login!");
+			}
+		});
 	}
 }
