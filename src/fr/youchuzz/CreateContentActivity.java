04673cd674092454dd959107396df1089aad52a3
Neamar <neamar@neamar.fr>
2012-05-22 20:30:37 +0200

    Content picking from gallery

--- src/fr/youchuzz/CreateContentActivity.java
+++ src/fr/youchuzz/CreateContentActivity.java
@@ -1,21 +1,30 @@
 package fr.youchuzz;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.androidquery.AQuery;
 
 public class CreateContentActivity extends BaseActivity {
+	
 	private int CONTENT_1 = 1;
 	private int CONTENT_2 = 2;
 	
 	private int PICKING = 0;
 	private int UPLOADING = 1;
 	private int PREVIEWING = 2;
+	
+	private int REQUEST_IMAGE = 100;
+	private int REQUEST_PHOTO = 101;
+	
+	private int currentlyPicking = 0;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -36,8 +45,6 @@ public class CreateContentActivity extends BaseActivity {
 		
 		registerForContextMenu(aq.id(R.id.create_content_add_1).getView());
 		registerForContextMenu(aq.id(R.id.create_content_add_2).getView());
-		
-		//http://developer.android.com/reference/android/content/Intent.html#ACTION_GET_CONTENT
 	}
 	
 	/**
@@ -46,16 +53,54 @@ public class CreateContentActivity extends BaseActivity {
 	 */
 	public void onPickContent(View v)
 	{
-		Log.i("yc", "Picking content");
+		if(v.getId() == R.id.create_content_add_1)
+			currentlyPicking = CONTENT_1;
+		else if(v.getId() == R.id.create_content_add_2)
+			currentlyPicking = CONTENT_2;
+		
 		v.showContextMenu();
 	}
 	
+	/**
+	 * Display "select pick menu"
+	 */
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
 		super.onCreateContextMenu(menu, v, menuInfo);
 		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.content_source_picker, menu);
+		inflater.inflate(R.menu.menu_content_source_picker, menu);
 	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.menu_content_source_pick_gallery:
+				//Pick from gallery (or other file browser)
+				Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+				galleryIntent.setType("image/*");
+				startActivityForResult(Intent.createChooser(galleryIntent, getString(R.string.create_content_pick_image)), REQUEST_IMAGE);
+
+				return true;
+			case R.id.menu_content_source_pick_photo:
+				//Pick by taking new picture
+				return true;
+			default:
+				return super.onContextItemSelected(item);
+		}
+	}
+	
+
+	public void onActivityResult (int requestCode, int resultCode, Intent data)
+	{
+		Log.i("yc", "Results for " + Integer.toString(requestCode));
+		if (resultCode == RESULT_OK) {
+			if (requestCode == REQUEST_IMAGE) {
+				Uri selectedImageUri = data.getData();
+				updateUi(currentlyPicking, UPLOADING);
+			}
+		}
+	}
+	
 	
 	/**
 	 * Update the UI to match current state
